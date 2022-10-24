8b7fe0f58435fbd1745a226558934e3090df45e0
hyunjun Yang <yhj0439@nate.com>
2022-10-24 17:37:07 +0900

    storeDetailPage 수정 => axios 분리, view 컴포넌트 생성 후 화면 구성관련 기능 정리

---
+++ front/src/Page/StorePage/storeDetailPage.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { useLocation, Link } from "react-router-dom";
+import { RiStarFill } from "react-icons/ri";
+
+import Header from "../ViewHeader";
+import Button from "../../component/Button";
+import StoreMap from "../../component/StoreMap";
+import GoodButton from "../../component/GoodButton.js";
+
+const StoreDetail = () => {
+  return;
+};
+
+export default StoreDetail;
